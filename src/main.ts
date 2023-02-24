773b1a3074570b2cba2469ec03aaae986e0ca4d8
padosum <makalu171@gmail.com>
2023-02-24 23:02:39 +0900

    ♻️ vuetify 설정 파일 분리
    - vue app에 적용

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,7 @@ import App from './App.vue';
 import router from './router';
 import 'vuetify/styles';
 import '@mdi/font/css/materialdesignicons.css';
-import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
-
-const vuetify = createVuetify({
-  components,
-  directives,
-});
+import vuetify from '@/utils/setupVuetify';
 
 const app = createApp(App);
 
