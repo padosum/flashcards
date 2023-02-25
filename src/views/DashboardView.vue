da20aab8f215e937d3c3a8df917938f29dfadef0
padosum <makalu171@gmail.com>
2023-02-26 08:46:21 +0900

    ✨ md 파일 내용을  localStorage에 저장
    - vueuse useStorage 사용

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -6,18 +6,24 @@
         :learnset-title="mdFile.name"
         title="카드 뭉치 추가하기"
         ref="modal"
+        @add-learnset="addLearnset"
       />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import markdownit from 'markdown-it';
 import fileDialog from 'file-dialog';
+import { useStorage } from '@vueuse/core';
 
 import BaseButton from '@/components/BaseButton.vue';
 import AddLearnsetModal from '@/components/AddLearnsetModal.vue';
+
 import { ref, reactive } from 'vue';
-import type { File } from '@/types/interfaces';
+import type { Ref } from 'vue';
+import type { File, Learnset } from '@/types/interfaces';
+import { getLearnsetFromTokens } from '@/utils/learnset';
 
 const mdFile: File = reactive({
   name: '',
@@ -43,11 +49,21 @@ const openDialog = async (): Promise<File> => {
     reader.readAsText(file);
     reader.onload = () =>
       resolve({
-        name: file.name,
+        name: file.name.replace(/\.[^/.]+$/, ''),
         contents: reader.result?.toString(),
       });
     reader.onerror = () => reject(new Error('No file selected'));
   });
+};
+
+const learnsets: Ref<Learnset[]> = useStorage('learnsets', []);
+
+const addLearnset = (name: string) => {
+  const md = markdownit({});
+  const tokens = md.parse(mdFile.contents as string, {});
+  const { cards, id, created } = getLearnsetFromTokens(tokens);
+
+  learnsets.value = [...learnsets.value, { cards, id, created, name }];
 };
 </script>
 
