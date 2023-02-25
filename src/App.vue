b4ca1f5067e4e95c958634aace1f1de8a38b7321
padosum <makalu171@gmail.com>
2023-02-25 14:59:49 +0900

    ✅ test가 통과되도록 수정
    - router name 렌더링

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-
-const drawer = ref(false);
-</script>
-
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer"> </v-navigation-drawer>
+    <v-navigation-drawer v-model="drawer">
+      <div class="text-h4 font-weight-black text-center py-3">FlashMD</div>
+      <v-divider></v-divider>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-app-bar>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>{{ pageTitle }}</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
@@ -18,5 +18,14 @@ const drawer = ref(false);
     </v-main>
   </v-app>
 </template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import { useRoute } from 'vue-router';
+const route = useRoute();
+
+const drawer = ref(true);
+const pageTitle = computed(() => route.name);
+</script>
 
 <style scoped></style>
--- tests/unit/router.spec.ts
+++ tests/unit/router.spec.ts
@@ -6,9 +6,10 @@ import vuetify from '@/utils/setupVuetify';
 
 describe('routing', () => {
   it('처음 url에 접속했을 때 dashboard page가 렌더링된다.', async () => {
+    const name = 'Dashboard';
     const router = createRouter({
       history: createWebHistory(),
-      routes: [{ path: '/', component: DashboardView }],
+      routes: [{ path: '/', component: DashboardView, name }],
     });
 
     const { findByText } = render(App, {
@@ -17,6 +18,6 @@ describe('routing', () => {
       },
     });
 
-    expect(await findByText('Dashboard')).toBeInTheDocument();
+    expect(await findByText(name)).toBeInTheDocument();
   });
 });
