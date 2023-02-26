bc84eb208b3a2a7c9dc3cdbe730da3618d64ec7b
padosum <makalu171@gmail.com>
2023-02-26 17:37:08 +0900

    🧪 NavigationDrawer 구현을 위한 test 추가
    - router.spect.ts store 전달

--- src/router/__tests__/router.spec.ts
+++ src/router/__tests__/router.spec.ts
@@ -3,18 +3,21 @@ import App from '@/App.vue';
 import DashboardView from '@/views/DashboardView.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import vuetify from '@/utils/setupVuetify';
+import type { RootState } from '@/store/state';
+import { createStore, type StoreOptions } from 'vuex';
+import { store } from '@/store';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [{ path: '/', component: DashboardView, name: 'Dashboard' }],
+});
 
 describe('routing', () => {
   it('처음 url에 접속했을 때 dashboard page가 렌더링된다.', async () => {
-    const name = 'Dashboard';
-    const router = createRouter({
-      history: createWebHistory(),
-      routes: [{ path: '/', component: DashboardView, name }],
-    });
-
+    const storeInstance = createStore(store);
     const { getByTestId } = render(App, {
       global: {
-        plugins: [router, vuetify],
+        plugins: [router, storeInstance, vuetify],
       },
     });
 
@@ -22,4 +25,43 @@ describe('routing', () => {
       expect(getByTestId('toolbar-title')).toHaveTextContent('Dashboard');
     });
   });
+
+  // it('navigation 링크 클릭시 해당되는 카드 뭉치 이름이 렌더링된다.', async () => {
+  //   const learnsets: Learnset[] = [
+  //     {
+  //       cards: [],
+  //       created: 1677390326088,
+  //       id: 'a',
+  //       name: 'cards 1',
+  //     },
+  //     {
+  //       cards: [],
+  //       created: 1677390326088,
+  //       id: 'b',
+  //       name: 'cards 2',
+  //     },
+  //   ];
+
+  //   const { getByText, getByTestId } = renderVuexApp({
+  //     state: {
+  //       learnsets,
+  //     },
+  //   });
+
+  //   await fireEvent.click(getByText('cards 1'));
+
+  //   await waitFor(() => {
+  //     expect(getByTestId('toolbar-title')).toHaveTextContent('cards 1');
+  //   });
+  // });
 });
+
+const renderVuexApp = (customStore: StoreOptions<RootState>) => {
+  const mergedStoreInstance = createStore({ ...store, ...customStore });
+
+  return render(App, {
+    global: {
+      plugins: [router, mergedStoreInstance, vuetify],
+    },
+  });
+};
