228c5c8f5f1eaf5b6d175e08ff9bb0ea9e62a6cb
padosum <makalu171@gmail.com>
2023-02-27 15:42:30 +0900

    ✅ Vuex getter 수정 및 추가
    - 기준날짜 이전에 학습해야 하는 데이터 조회

--- src/store/__tests__/getters.spec.ts
+++ src/store/__tests__/getters.spec.ts
@@ -1,3 +1,5 @@
+import type { Learnset } from '@/types/interfaces';
+import Token from 'markdown-it/lib/token';
 import { getters } from '../getters';
 
 describe('getters', () => {
@@ -22,5 +24,57 @@ describe('getters', () => {
     const actual = getters.learnset(state, '1');
 
     expect(actual).toEqual([learnsets[0]]);
+  });
+
+  it('get learnset by date', () => {
+    const TEMP_TOKEN = new Token('', '', 0);
+    const CARD1 = {
+      id: '1',
+      front: [TEMP_TOKEN],
+      back: [TEMP_TOKEN],
+      title: '안녕하세요',
+      efactor: 2.5,
+      dueDate: new Date('2023-02-25').toISOString(),
+      repetition: 0,
+      interval: 0,
+      reviewDate: '',
+    };
+
+    const CARD2 = {
+      id: '1',
+      front: [TEMP_TOKEN],
+      back: [TEMP_TOKEN],
+      title: '안녕하세요',
+      efactor: 2.5,
+      dueDate: new Date('2023-02-27').toISOString(),
+      repetition: 0,
+      interval: 0,
+      reviewDate: '',
+    };
+
+    const learnsets: Learnset[] = [
+      {
+        cards: [CARD1, CARD2],
+        created: 0,
+        id: '1',
+        name: 'learnset 1',
+      },
+      {
+        cards: [],
+        created: 0,
+        id: '2',
+        name: 'learnset 2',
+      },
+    ];
+
+    const state = { learnsets };
+
+    const [learnset] = getters.learnset(state, '1');
+
+    const BASE_DATE = new Date('2023-02-27').getTime();
+    const actual = getters.reviewCards(state, learnset)(BASE_DATE);
+    const reviewCards = learnsets[0];
+    reviewCards.cards = [CARD1];
+    expect(actual).toEqual(reviewCards);
   });
 });
--- src/store/getters.ts
+++ src/store/getters.ts
@@ -1,9 +1,21 @@
+import type { Card, Learnset } from '@/types/interfaces';
 import type { RootState } from './state';
 
 export const getters = {
-  learnset: (state: RootState, id: string) => {
+  learnset: (state: RootState, id: string): Learnset[] => {
     return state.learnsets.filter((learnset) => learnset.id === id);
   },
+  reviewCards:
+    (state: RootState, learnset: Learnset) =>
+    (baseDate = new Date().getTime()): Learnset => {
+      const cards: Card[] = learnset.cards.filter(
+        (card: Card) => new Date(card.dueDate).getTime() < baseDate
+      );
+
+      learnset.cards = cards;
+
+      return learnset;
+    },
 };
 
 export type Getters = typeof getters;
