1141d7e9a3e8274436cbb5ab0d48495a073121d8
padosum <makalu171@gmail.com>
2023-02-28 16:30:28 +0900

    ✨ /learnset route에서 /learn 로 이동하는 버튼추가

--- src/views/LearnsetView.vue
+++ src/views/LearnsetView.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>hello</div>
+  <v-btn variant="outlined" :to="`/learn/${route.params.id}`">
+    학습 시작하기</v-btn
+  >
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+const route = useRoute();
+</script>
 
 <style scoped></style>
